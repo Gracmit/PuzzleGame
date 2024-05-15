55f15046b7c33bcc1065dfbe1abcbe822b84b17e
Gracmit <aleksii97@gmail.com>
2024-05-15 14:36:50 +0300

    5 more levels

--- Puzzle Game/Assets/Scripts/GameButton.cs
+++ Puzzle Game/Assets/Scripts/GameButton.cs
@@ -58,7 +58,7 @@ public class GameButton : MonoBehaviour, IPointerClickHandler
             _holder = movableHolder;
             _positionToMove = _holder.transform.position;
             _holder.SetGameButton(this);
-            AudioManager.Instance.PlayMoveSFX();
+            //AudioManager.Instance.PlayMoveSFX();
             //GameManager.Instance.CheckForVictoryCondition();
         }
     }
